6ab0c2192ae8b010d20958424eb7d7612eab87ef
Allison Codi <allison.codi@emory.edu>
2025-06-02 16:09:19 -0400

    added bootstrap for new versions

--- R/alt_tests.R
+++ R/alt_tests.R
@@ -293,7 +293,7 @@ hudgens_test <- function(
 #' @param G_name growth outcome variable name
 #' @param V_name vaccination variable name
 #' @param Y_name infection variable name
-#' @param family gaussian for continuous outcome, binomial for binary
+#' @param family gaussian for continuous outcome, binomial for binary outcome
 #' 
 #' @returns Hudgens-style test statistic
 get_hudgens_stat_new <- function(
@@ -428,6 +428,96 @@ get_hudgens_stat_new <- function(
   
   return(out)
 
+}
+
+#' Function for bootstrap test of Hudgens-style test statistic
+#' 
+#' @param data dataframe containing dataset to use for analysis
+#' @param G_name growth outcome variable name
+#' @param V_name vaccination variable name
+#' @param Y_name infection variable name
+#' @param family gaussian for continuous outcome, binomial for binary
+#' @param n_boot number of bootstrap replicates
+#' @param n_boot_try max number of attempts for bootstrap resampling
+#' @param effect_dir direction of beneficial effect, defaults to "positive" for beneficial outcome. Used for one-side tests of bounds.  
+#'  
+#' @returns list with observed difference between plc and vax, Hudgens-style test statistic, and p-value
+hudgens_test_new <- function(
+    data, 
+    G_name = "G",
+    V_name = "V",
+    Y_name = "Y",
+    family = "gaussian",
+    n_boot = 1e3, 
+    n_boot_try = n_boot*10,
+    effect_dir = "positive"
+){
+  
+  n <- nrow(data)
+  
+  # Step 1: rhobar_z_n
+  rhobar_0_n <- mean(data[[Y_name]][data[[V_name]] == 0])
+  rhobar_1_n <- mean(data[[Y_name]][data[[V_name]] == 1])
+
+  if(rhobar_0_n > rhobar_1_n){
+    
+    out <- get_hudgens_stat_new(
+      data = data, Y_name = Y_name, G_name = G_name, V_name = V_name, family = family
+    )
+    
+    boot_diff <- vector(mode = "list", length = n_boot)
+    success_ct <- 0
+    attempt_ct <- 0
+    while(success_ct < n_boot & attempt_ct <= n_boot_try){
+      attempt_ct <- attempt_ct + 1
+      boot_idx <- sample(seq_len(n), replace = TRUE)
+      boot_data <- data[boot_idx, , drop = FALSE]
+      
+      rhobar_0_n_boot <- mean(boot_data[[Y_name]][boot_data[[V_name]] == 0])
+      rhobar_1_n_boot <- mean(boot_data[[Y_name]][boot_data[[V_name]] == 1])
+      
+      if(rhobar_0_n_boot > rhobar_1_n_boot){
+        success_ct <- success_ct + 1
+        boot_diff[[success_ct]] <- get_hudgens_stat_new(
+          data = boot_data, Y_name = Y_name, G_name = G_name, V_name = V_name, family = family
+        )
+      }
+    }
+    if(sum(!is.na(boot_diff)) != n_boot){
+      warning(paste0(
+        "Only achieved ", sum(!is.na(boot_diff)), " successful resamples. Try again with larger n_boot_try? \n sample size: ", nrow(data)
+      ))
+    }
+    
+    if(sum(!is.na(boot_diff)) > 2){
+      
+      boot_diff_res <- data.frame(do.call(rbind, boot_diff))
+      
+      sd_boot_diff_additive_lower <- sd(unlist(boot_diff_res$additive_effect_lower), na.rm = TRUE)
+      sd_boot_diff_additive_upper <- sd(unlist(boot_diff_res$additive_effect_upper), na.rm = TRUE)
+      sd_boot_diff_mult_lower <- sd(unlist(boot_diff_res$mult_effect_lower), na.rm = TRUE)
+      sd_boot_diff_mult_upper <- sd(unlist(boot_diff_res$mult_effect_upper), na.rm = TRUE)
+
+      out <- c(
+        out,
+        list(
+          sd_additive_lower = sd_boot_diff_additive_lower,
+          sd_additive_upper = sd_boot_diff_additive_upper,
+          sd_mult_lower = sd_boot_diff_mult_lower,
+          sd_mult_upper = sd_boot_diff_mult_upper
+        )
+      )
+      
+      
+      class(out) <- "hudgens_res_new"
+      return(out)
+    }else{
+      stop("Did not achieve 2 successful bootstrap resamples. Try again with larger n_boot_try? Or give up?")
+    }
+    
+  }else{
+    stop("Method not applicable unless evidence of vaccine protection.")
+  }
 }
 
 #' Function for hudgens-style test statistic - match description in the paper
@@ -622,6 +712,96 @@ get_hudgens_stat_doomed <- function(
   
   return(mean_G_vax - mean_G_plc)
   
+}
+
+#' Function for bootstrap test of Hudgens-style test statistic
+#' 
+#' @param data dataframe containing dataset to use for analysis
+#' @param G_name growth outcome variable name
+#' @param V_name vaccination variable name
+#' @param Y_name infection variable name
+#' @param family gaussian for continuous outcome, binomial for binary
+#' @param n_boot number of bootstrap replicates
+#' @param n_boot_try max number of attempts for bootstrap resampling
+#' @param effect_dir direction of beneficial effect, defaults to "positive" for beneficial outcome. Used for one-side tests of bounds.  
+#'  
+#' @returns list with observed difference between plc and vax, Hudgens-style test statistic, and p-value
+hudgens_test_doomed_new <- function(
+    data, 
+    G_name = "G",
+    V_name = "V",
+    Y_name = "Y",
+    family = "gaussian",
+    n_boot = 1e3, 
+    n_boot_try = n_boot*10,
+    effect_dir = "positive"
+){
+  
+  n <- nrow(data)
+  
+  # Step 1: rhobar_z_n
+  rhobar_0_n <- mean(data[[Y_name]][data[[V_name]] == 0])
+  rhobar_1_n <- mean(data[[Y_name]][data[[V_name]] == 1])
+  
+  if(rhobar_0_n > rhobar_1_n){
+    
+    out <- get_hudgens_stat_doomed_new(
+      data = data, Y_name = Y_name, G_name = G_name, V_name = V_name, family = family
+    )
+    
+    boot_diff <- vector(mode = "list", length = n_boot)
+    success_ct <- 0
+    attempt_ct <- 0
+    while(success_ct < n_boot & attempt_ct <= n_boot_try){
+      attempt_ct <- attempt_ct + 1
+      boot_idx <- sample(seq_len(n), replace = TRUE)
+      boot_data <- data[boot_idx, , drop = FALSE]
+      
+      rhobar_0_n_boot <- mean(boot_data[[Y_name]][boot_data[[V_name]] == 0])
+      rhobar_1_n_boot <- mean(boot_data[[Y_name]][boot_data[[V_name]] == 1])
+      
+      if(rhobar_0_n_boot > rhobar_1_n_boot){
+        success_ct <- success_ct + 1
+        boot_diff[[success_ct]] <- get_hudgens_stat_doomed_new(
+          data = boot_data, Y_name = Y_name, G_name = G_name, V_name = V_name, family = family
+        )
+      }
+    }
+    if(sum(!is.na(boot_diff)) != n_boot){
+      warning(paste0(
+        "Only achieved ", sum(!is.na(boot_diff)), " successful resamples. Try again with larger n_boot_try? \n sample size: ", nrow(data)
+      ))
+    }
+    
+    if(sum(!is.na(boot_diff)) > 2){
+      
+      boot_diff_res <- data.frame(do.call(rbind, boot_diff))
+      
+      sd_boot_diff_additive_lower <- sd(unlist(boot_diff_res$additive_effect_lower), na.rm = TRUE)
+      sd_boot_diff_additive_upper <- sd(unlist(boot_diff_res$additive_effect_upper), na.rm = TRUE)
+      sd_boot_diff_mult_lower <- sd(unlist(boot_diff_res$mult_effect_lower), na.rm = TRUE)
+      sd_boot_diff_mult_upper <- sd(unlist(boot_diff_res$mult_effect_upper), na.rm = TRUE)
+      
+      out <- c(
+        out,
+        list(
+          sd_additive_lower = sd_boot_diff_additive_lower,
+          sd_additive_upper = sd_boot_diff_additive_upper,
+          sd_mult_lower = sd_boot_diff_mult_lower,
+          sd_mult_upper = sd_boot_diff_mult_upper
+        )
+      )
+      
+      
+      class(out) <- "hudgens_res_doomed_new"
+      return(out)
+    }else{
+      stop("Did not achieve 2 successful bootstrap resamples. Try again with larger n_boot_try? Or give up?")
+    }
+    
+  }else{
+    stop("Method not applicable unless evidence of vaccine protection.")
+  }
 }
 
 #' Function for bootstrap test of Hudgens-style test statistic - doomed
